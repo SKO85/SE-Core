c9e5f000e911163d67f20ac3158b3050870e2716
SKO85 <SKO85-gaming@gmail.com>
2019-11-24 15:42:30 +0100

    MOdified Actions and Arguments added to split the functionality. Arguments are now not provided as parameter to the Run(...) method.

---
+++ Work/Arguments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKO85Core.Work
+{
+    public static class Arguments
+    {
+        public static Dictionary<string, Action> ActionHandlers = new Dictionary<string, Action>();
+
+        public static void On(string actionName, Action actionCallback)
+        {
+            ActionHandlers[actionName] = actionCallback;
+        }
+
+        public static void Run(string actionName)
+        {
+            if (ActionHandlers.ContainsKey(actionName))
+            {
+                ActionHandlers[actionName]();
+            }
+        }
+    }
+}
